08155c2ecdd0abab6ea129eb4c64548b5ca6087a
yunsaturn <yunohsung0925@gmail.com>
2026-04-08 09:58:07 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/bye", (req, res) => {
+    res.send("<h1>Goodbye, World!</h1>");
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 3000!');
+});
